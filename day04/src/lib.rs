4701cbb959803b37c0b6ab4e7dfb863f6bde9025
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-06-18 20:29:52 +0200

    rewrite day04 part 2 in modern style

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -1,8 +1,8 @@
 use aoclib::parse;
-
-use std::{collections::HashMap, path::Path};
+use std::{collections::HashMap, convert::TryInto, path::Path};
 
 type Id = u32;
+type Minute = u32;
 
 #[derive(
     Clone,
@@ -61,8 +61,13 @@ struct LogEntry {
     action: Action,
 }
 
-fn minutes_asleep(logs: &[LogEntry]) -> HashMap<Id, u32> {
-    let mut sleep_time = HashMap::new();
+type AsleepByMinute = [HashMap<Id, Minute>; 60];
+
+/// Produce a data structure recording for each minute, how many times each guard was asleep that minute.
+fn asleep_by_minute(logs: &[LogEntry]) -> AsleepByMinute {
+    let asleep = vec![HashMap::default(); 60];
+    let mut asleep: [HashMap<_, _>; 60] =
+        asleep.try_into().expect("just initialized; never changed");
     let mut guard = None;
     let mut sleep_start = None;
 
@@ -79,65 +84,87 @@ fn minutes_asleep(logs: &[LogEntry]) -> HashMap<Id, u32> {
             Action::WakeUp => {
                 let sleep_start = sleep_start.take().expect("can't wake if not asleep");
                 let sleep_end = entry.timestamp.minute;
-                *sleep_time
-                    .entry(guard.expect("can't wake if not a guard"))
-                    .or_default() += (sleep_end - sleep_start) as u32;
-            }
-        }
-    }
-
-    sleep_time
-}
-
-fn sleepiest_minute(guard: Id, logs: &[LogEntry]) -> u32 {
-    let mut minutes_slept = [0; 60];
-    let mut on_shift = false;
-    let mut sleep_start = None;
-
-    for entry in logs {
-        match entry.action {
-            Action::BeginShift(id) => on_shift = id == guard,
-            Action::FallAsleep if on_shift => {
-                debug_assert!(sleep_start.is_none());
-                sleep_start = entry.timestamp.minute.into();
-            }
-            Action::WakeUp if on_shift => {
-                let sleep_start = sleep_start.take().expect("can't wake if not asleep");
-                let sleep_end = entry.timestamp.minute;
+                let guard = guard.expect("can't wake if not a guard");
                 for minute in sleep_start..sleep_end {
-                    minutes_slept[minute as usize] += 1;
+                    *asleep[minute as usize].entry(guard).or_default() += 1;
                 }
             }
-            _ => {}
         }
     }
 
-    std::array::IntoIter::new(minutes_slept)
-        .enumerate()
-        .map(|(idx, time_slept)| (time_slept, idx))
+    asleep
+}
+
+/// count total minutes asleep per guard
+fn total_minutes_by_guard(by_minute: &AsleepByMinute) -> HashMap<Id, Minute> {
+    by_minute.iter().fold(HashMap::new(), |mut acc, elem| {
+        for (guard, times_slept) in elem {
+            *acc.entry(*guard).or_default() += times_slept;
+        }
+        acc
+    })
+}
+
+/// get guard with most minutes asleep
+fn sleepiest_guard(by_guard: &HashMap<Id, Minute>) -> Option<Id> {
+    by_guard
+        .iter()
+        .map(|(&id, &slept)| (slept, id))
         .max()
-        .map(|(_, idx)| idx as u32)
-        .unwrap()
+        .map(|(_, id)| id)
+}
+
+/// get minute with most instances asleep for a guard
+fn sleepiest_minute(guard: Id, by_minute: &AsleepByMinute) -> Minute {
+    by_minute
+        .iter()
+        .enumerate()
+        .map(|(minute, map)| (map.get(&guard).copied().unwrap_or_default(), minute))
+        .max()
+        .map(|(_, minute)| minute as Minute)
+        .expect("60 elements; never empty")
+}
+
+/// get guard most frequently asleep on the same minute
+fn most_freq_asleep_per_minute(by_minute: &AsleepByMinute) -> Option<(Id, Minute)> {
+    by_minute
+        .iter()
+        .enumerate()
+        .flat_map(|(minute, by_guard)| {
+            by_guard
+                .iter()
+                .map(move |(&guard, &minutes_asleep)| (minutes_asleep, guard, minute as Minute))
+        })
+        .max()
+        .map(|(_, guard, minute)| (guard, minute))
 }
 
 pub fn part1(input: &Path) -> Result<(), Error> {
     let mut logs: Vec<LogEntry> = parse(input)?.collect();
     logs.sort_unstable();
 
-    let sleep_times = minutes_asleep(&logs);
-    let sleepiest_guard = sleep_times
-        .iter()
-        .map(|(&id, &slept)| (slept, id))
-        .max()
-        .map(|(_, id)| id)
-        .ok_or(Error::NoSolution)?;
-    let sleepiest_minute = sleepiest_minute(sleepiest_guard, &logs);
-    dbg!(sleepiest_guard, sleepiest_minute, sleepiest_guard * sleepiest_minute);
+    let by_minute = asleep_by_minute(&logs);
+    let sleep_times = total_minutes_by_guard(&by_minute);
+    let sleepiest_guard = sleepiest_guard(&sleep_times).ok_or(Error::NoSolution)?;
+    let sleepiest_minute = sleepiest_minute(sleepiest_guard, &by_minute);
+    println!("part 1");
+    dbg!(
+        sleepiest_guard,
+        sleepiest_minute,
+        sleepiest_guard * sleepiest_minute
+    );
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let mut logs: Vec<LogEntry> = parse(input)?.collect();
+    logs.sort_unstable();
+
+    let by_minute = asleep_by_minute(&logs);
+    let (guard, minute) = most_freq_asleep_per_minute(&by_minute).ok_or(Error::NoSolution)?;
+    println!("part 2");
+    dbg!(guard, minute, guard * minute);
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
