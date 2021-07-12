d64d1ce285993e9e0b8bff50bb86e3fb2db46ea2
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-07-12 15:50:28 +0200

    rewrite day05 part1 again using two pointers, not linked list

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,4 +1,5 @@
-use aoclib::{data_structures::linked_list::LinkedList, parse};
+use aoclib::parse;
+use bitvec::{bitvec, order::LocalBits};
 use std::{path::Path, string::FromUtf8Error};
 
 fn reacts(a: u8, b: u8) -> bool {
@@ -7,43 +8,62 @@ fn reacts(a: u8, b: u8) -> bool {
     a != b && a.eq_ignore_ascii_case(&b)
 }
 
-fn react_to_completion(data: impl IntoIterator<Item = u8>) -> Vec<u8> {
-    let mut list: LinkedList<_> = data.into_iter().collect();
+/// Perform the entire reaction in a single pass, using two pointers into the
+/// input data.
+fn react_to_completion(data: &[u8]) -> Vec<u8> {
+    if data.len() < 2 {
+        return data.into();
+    }
 
-    // keep looping through the list as long as the length keeps changing
-    let mut len_before = !0;
-    while len_before != list.len() {
-        len_before = list.len();
-        let mut cursor = list.cursor_front();
+    // vector of items which have been reacted / excluded
+    let mut exclusions = bitvec![LocalBits, u32; 0; data.len()];
 
-        // for each cursor position, if there is a match with the successor,
-        // clear this position and the next
-        loop {
-            match (cursor.elem().copied(), cursor.peek_next().copied()) {
-                (Some(elem), Some(next)) if reacts(elem, next) => {
-                    dbg!(elem as char, next as char);
-                    // clear the members of the reaction
-                    // taking moves the cursor forward if possible
-                    cursor.take();
-                    cursor.take();
-                }
-                (Some(_), Some(_)) => {
-                    // these aren't reactive chemicals, advance the cursor
-                    dbg!(cursor.advance().map(|byte| *byte as char));
-                }
-                _ => {
-                    // we're at the end of the list
-                    break;
-                }
+    // two pointers into the data
+    let mut lead = 1;
+    let mut trail = 0;
+
+    // this is worst case of O(n**2) in the event that all elements are excluded.
+    // the simplest way to accomplish that is to ensure that all adjacent elements pair each other.
+    // however, I'm pretty confident that the actual length of runs of reactions in the input will
+    // be short enough that we don't mind.
+    while lead < data.len() {
+        // neither lead nor trail must currently be excluded
+        debug_assert!(!exclusions[lead]);
+        debug_assert!(!exclusions[trail]);
+
+        if reacts(data[lead], data[trail]) {
+            // exclude both of these elements; they reacted away
+            exclusions.set(lead, true);
+            exclusions.set(trail, true);
+
+            // advance the lead
+            lead += 1;
+
+            // trail backs up to the most recent non-excluded char
+            while trail > 0 && exclusions[trail] {
+                trail -= 1;
             }
+            // if we run out of chars, we might need to reset entirely
+            if trail == 0 && exclusions[trail] {
+                trail = lead;
+                lead += 1;
+            }
+        } else {
+            trail = lead;
+            lead += 1;
         }
     }
 
-    list.iter().copied().collect()
+    // the output is the input minus all exclusions
+    data.iter()
+        .copied()
+        .zip(exclusions.into_iter())
+        .filter_map(|(byte, excluded)| (!excluded).then(move || byte))
+        .collect()
 }
 
 fn react_str(polymer: String) -> Result<String, Error> {
-    String::from_utf8(react_to_completion(polymer.as_bytes().iter().copied())).map_err(Into::into)
+    String::from_utf8(react_to_completion(polymer.as_bytes())).map_err(Into::into)
 }
 
 pub fn part1(input: &Path) -> Result<(), Error> {
