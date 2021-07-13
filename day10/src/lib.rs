d6fe38ac772d302278f485c4670fd9f3abe9074c
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-07-13 15:50:25 +0200

    rewrite day10 in modern style

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,179 +1,168 @@
-use failure::Fail;
-use itertools::Itertools;
+use aoclib::{
+    geometry::{tile::Bool, Map, Point},
+    parse,
+};
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::fmt;
-use std::num::ParseIntError;
-use std::ops::Add;
-use std::str::FromStr;
+use std::{num::ParseIntError, path::Path, str::FromStr};
 
 lazy_static! {
-    static ref POINT_RE: Regex =
-        Regex::new(r"position=(?P<position><\s?(?P<px>-?\d+), \s?(?P<py>-?\d+)>) velocity=(?P<velocity><\s?(?P<vx>-?\d+), \s?(?P<vy>-?\d+)>)").unwrap();
+    static ref POINT_RE: Regex = Regex::new(r"<\s?(?P<x>-?\d+), \s?(?P<y>-?\d+)>").unwrap();
+    static ref LIGHT_RE: Regex =
+        Regex::new(r"position=(?P<position><[-\d ,]+>) velocity=(?P<velocity><[-\d ,]+>)").unwrap();
 }
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
-struct Vec2 {
-    x: i64,
-    y: i64,
+fn parse_point(s: &str) -> Result<Point, Error> {
+    let captures = POINT_RE.captures(s).ok_or(Error::ParseError)?;
+    let x = captures
+        .name("x")
+        .expect("x always in captures")
+        .as_str()
+        .parse()?;
+    let y = captures
+        .name("y")
+        .expect("y always in captures")
+        .as_str()
+        .parse()?;
+    Ok(Point::new(x, y))
 }
 
-impl Add for Vec2 {
-    type Output = Vec2;
+#[derive(Clone, Copy, Debug)]
+struct Light {
+    position: Point,
+    velocity: Point,
+}
 
-    fn add(self, other: Self) -> Vec2 {
-        Vec2 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
+impl FromStr for Light {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let captures = LIGHT_RE.captures(s).ok_or(Error::ParseError)?;
+        let position = captures
+            .name("position")
+            .expect("position always in captures")
+            .as_str();
+        let position = parse_point(position)?;
+        let velocity = captures
+            .name("velocity")
+            .expect("velocity always in captures")
+            .as_str();
+        let velocity = parse_point(velocity)?;
+
+        Ok(Light { position, velocity })
     }
 }
 
-impl fmt::Display for Vec2 {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "<{:2}, {:2}>", self.x, self.y)
+/// Compute the `(min, max)` bounds enclosing the given points.
+fn bounds(points: &[Light]) -> (Point, Point) {
+    let mut min_x: Option<i32> = None;
+    let mut max_x: Option<i32> = None;
+    let mut min_y: Option<i32> = None;
+    let mut max_y: Option<i32> = None;
+
+    macro_rules! update {
+        ($value:ident <= $op:ident($field:expr)) => {
+            $value = match $value {
+                None => Some($field),
+                Some(v) => Some(v.$op($field)),
+            };
+        };
     }
-}
-
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
-pub struct Point {
-    position: Vec2,
-    velocity: Vec2,
-}
-
-#[derive(Debug, Fail)]
-pub enum ParsePointError {
-    #[fail(display = "capture not found")]
-    CaptureNotFound,
-    #[fail(display = "parse int error: {}", _0)]
-    ParseInt(#[cause] ParseIntError),
-}
-
-impl From<ParseIntError> for ParsePointError {
-    fn from(err: ParseIntError) -> ParsePointError {
-        ParsePointError::ParseInt(err)
-    }
-}
-
-impl FromStr for Point {
-    type Err = ParsePointError;
-
-    fn from_str(s: &str) -> Result<Point, Self::Err> {
-        let captures = POINT_RE
-            .captures(s)
-            .ok_or(ParsePointError::CaptureNotFound)?;
-        let px = captures
-            .name("px")
-            .ok_or(ParsePointError::CaptureNotFound)?
-            .as_str()
-            .parse()?;
-        let py = captures
-            .name("py")
-            .ok_or(ParsePointError::CaptureNotFound)?
-            .as_str()
-            .parse()?;
-        let vx = captures
-            .name("vx")
-            .ok_or(ParsePointError::CaptureNotFound)?
-            .as_str()
-            .parse()?;
-        let vy = captures
-            .name("vy")
-            .ok_or(ParsePointError::CaptureNotFound)?
-            .as_str()
-            .parse()?;
-
-        Ok(Point {
-            position: Vec2 { x: px, y: py },
-            velocity: Vec2 { x: vx, y: vy },
-        })
-    }
-}
-
-fn bounds(points: &[Point]) -> Option<(Vec2, Vec2)> {
-    let mut min_x = None;
-    let mut max_x = None;
-    let mut min_y = None;
-    let mut max_y = None;
 
     for point in points {
-        min_x = match min_x {
-            Some(cmin_x) if cmin_x < point.position.x => Some(cmin_x),
-            _ => Some(point.position.x),
-        };
-        min_y = match min_y {
-            Some(cmin_y) if cmin_y < point.position.y => Some(cmin_y),
-            _ => Some(point.position.y),
-        };
-        max_x = match max_x {
-            Some(cmax_x) if cmax_x > point.position.x => Some(cmax_x),
-            _ => Some(point.position.x),
-        };
-        max_y = match max_y {
-            Some(cmax_y) if cmax_y > point.position.y => Some(cmax_y),
-            _ => Some(point.position.y),
-        };
+        update!(min_x <= min(point.position.x));
+        update!(min_y <= min(point.position.y));
+        update!(max_x <= max(point.position.x));
+        update!(max_y <= max(point.position.y));
     }
 
-    Some((
-        Vec2 {
-            x: min_x?,
-            y: min_y?,
-        },
-        Vec2 {
-            x: max_x?,
-            y: max_y?,
-        },
-    ))
+    (
+        Point::new(min_x.unwrap_or_default(), min_y.unwrap_or_default()),
+        Point::new(max_x.unwrap_or_default(), max_y.unwrap_or_default()),
+    )
 }
 
-pub fn area(points: &[Point]) -> Option<i64> {
-    let (min, max) = bounds(points)?;
-    Some(((max.x - min.x) * (max.y - min.y)).abs())
+/// Compute the bounding area of the given points.
+fn area(points: &[Light]) -> u64 {
+    let (min, max) = bounds(points);
+    debug_assert!(max.x >= min.x);
+    debug_assert!(max.y >= min.y);
+    let width = (max.x - min.x) as u64;
+    let height = (max.y - min.y) as u64;
+    width * height
 }
 
-pub fn next_state(points: &[Point], next: &mut [Point]) {
-    for (idx, point) in points.iter().enumerate() {
-        next[idx] = Point {
-            position: point.position + point.velocity,
-            ..*point
-        };
+// advance the state of the lights
+fn tick(lights: &mut [Light]) {
+    for light in lights.iter_mut() {
+        light.position += light.velocity;
     }
 }
 
-pub fn find_min_area(points: &[Point]) -> (Vec<Point>, usize) {
-    use std::mem::swap;
+fn find_min_area(mut lights: Vec<Light>) -> (Vec<Light>, usize) {
     let mut count = 0;
 
-    let mut prev_state = points.to_vec();
-    let mut state = prev_state.clone();
+    let mut prev_state = lights.clone();
+    tick(&mut lights);
 
-    while area(&state) <= area(&prev_state) {
-        next_state(&mut state, &mut prev_state);
-        swap(&mut prev_state, &mut state);
+    while area(&lights) <= area(&prev_state) {
+        prev_state = lights.clone();
+        tick(&mut lights);
         count += 1;
     }
-
-    count -= 1;
 
     (prev_state, count)
 }
 
-pub fn display(points: &[Point]) -> Option<String> {
-    let (min, max) = bounds(points)?;
-
-    let mut out = vec![vec!['.'; (max.x - min.x + 1) as usize]; (max.y - min.y + 1) as usize];
-
-    for Point {
-        position: Vec2 { x, y },
-        ..
-    } in points
-    {
-        let x_idx = (x - min.x) as usize;
-        let y_idx = (y - min.y) as usize;
-
-        out[y_idx][x_idx] = '#';
+fn to_map(mut lights: Vec<Light>) -> Map<Bool> {
+    // adjust the lights such that the minimum corner is at `(0, 0)`
+    let (min, max) = bounds(&lights);
+    for light in lights.iter_mut() {
+        light.position -= min;
     }
 
-    Some(out.iter().map(|row| row.iter().join("")).join("\n"))
+    let max = max - min;
+
+    debug_assert_eq!(
+        bounds(&lights),
+        (Point::new(0, 0), max),
+        "adjustment must minimize positions",
+    );
+    // convert into a map
+    let mut map = Map::new(max.x as usize + 1, max.y as usize + 1);
+    for light in lights {
+        map[light.position] = Bool::True;
+    }
+
+    // map's origin is in bottom left, but AoC coords assume origin is in top left
+    map = map.flip_vertical();
+
+    map
+}
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    let lights: Vec<Light> = parse(input)?.collect();
+    let (min_area_lights, _) = find_min_area(lights);
+    let map = to_map(min_area_lights);
+    println!("{}", map);
+    Ok(())
+}
+
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let lights: Vec<Light> = parse(input)?.collect();
+    let (_, time_to_answer) = find_min_area(lights);
+    println!("time to answer: {}", time_to_answer);
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("Failed to parse input line as Light")]
+    ParseError,
+    #[error("Failed to parse a value as an integer")]
+    ParseIntError(#[from] ParseIntError),
+    #[error("No solution found")]
+    NoSolution,
 }
