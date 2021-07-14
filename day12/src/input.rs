5c0ef4456828765044bc4abe2fd78e910d227416
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-07-14 11:05:41 +0200

    replace lalrpop with pest

--- day12/src/input.rs
+++ day12/src/input.rs
@@ -1,33 +1,71 @@
+//! This module handles parsing the input.
+//
+// It's reliant on the `pest` parser generator, which I have mixed feelings about.
+// On the one hand, it's more powerful than LALRPOP; it Just Works on the input
+// for this day, which LALRPOP didn't even with some research and tweaking.
+//
+// On the other hand, it doesn't provide a very convenient interface; having parsed
+// the input, I now have to walk the parse tree manually and produce my desired types
+// by hand. That doesn't feel great, particularly when combined with this bit of the
+// documentation:
+//
+// > You _should_ rely on the maning of your grammar for properties such as "contains
+// > _n_ sub-rules", "is safe to `parse` to `f32`", and "never fails to match". Idiomatic
+// > `pest` code uses `unwrap` and `unreachable!`.
+//
+// That feels somewhat brittle; it's safe as long as all changes to `parser.pest`
+// are always synchronized with changes here.
+
 use crate::{encode_as_u8::EncodeAsU8, Error, Rules};
+use bitvec::vec::BitVec;
 use std::path::Path;
 
-pub type Rule = ([bool; 5], bool);
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "parser.pest"]
+pub struct InputParser;
 
 /// Representation of the day's input.
+#[derive(Debug)]
 pub struct Input {
-    pub initial: Vec<bool>,
-    pub rules: Vec<Rule>,
+    pub initial: BitVec,
+    pub rules: Rules,
 }
 
 impl Input {
     pub fn new(path: &Path) -> Result<Self, Error> {
         let input_data = std::fs::read_to_string(path)?;
-        crate::parser::InputParser::new()
-            .parse(&input_data)
-            .map_err(|err| {
-                err.map_token(|token| token.to_string())
-                    .map_error(|err| err.to_string())
-                    .into()
+        let mut file_pairs = InputParser::parse(Rule::file, &input_data)?
+            .next()
+            .unwrap()
+            .into_inner();
+        let initial: BitVec = file_pairs
+            .next()
+            .unwrap()
+            .into_inner()
+            .map(|rule| match rule.as_str() {
+                "." => false,
+                "#" => true,
+                _ => unreachable!(),
             })
-    }
+            .collect();
 
-    pub fn rules(&self) -> Rules {
         let mut rules = Rules::default();
-
-        for (rule, sets) in self.rules.iter().copied() {
-            rules[rule.as_u8() as usize] = sets;
+        for rule_def in file_pairs.next().unwrap().into_inner() {
+            let mut bits = [false; 5];
+            let mut bit_rule_iter = rule_def.into_inner();
+            for (idx, bit_rule) in bit_rule_iter.by_ref().take(5).enumerate() {
+                if bit_rule.as_str() == "#" {
+                    bits[idx] = true;
+                }
+            }
+            if bit_rule_iter.next().unwrap().as_str() == "#" {
+                rules[bits.as_u8() as usize] = true;
+            }
         }
 
-        rules
+        Ok(Self { initial, rules })
     }
 }
--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -3,16 +3,13 @@ mod input;
 
 use std::path::Path;
 
-use lalrpop_util::lalrpop_mod;
-lalrpop_mod!(#[allow(clippy::all)] pub parser); // synthesized by LALRPOP
-
 // each array of 5 bits corresponds to a single number in the range `0..32`,
 // so we can encode the complete ruleset as an array of 32 bools.
 pub type Rules = [bool; 32];
 
 pub fn part1(input: &Path) -> Result<(), Error> {
     let input = input::Input::new(input)?;
-    let rules = input.rules();
+    let rules = input.rules;
     unimplemented!()
 }
 
@@ -24,8 +21,8 @@ pub fn part2(_input: &Path) -> Result<(), Error> {
 pub enum Error {
     #[error(transparent)]
     Io(#[from] std::io::Error),
-    #[error("Failed to parse input")]
-    Parse(#[from] lalrpop_util::ParseError<usize, String, String>),
+    #[error(transparent)]
+    Parse(#[from] pest::error::Error<input::Rule>),
     #[error("No solution found")]
     NoSolution,
 }
