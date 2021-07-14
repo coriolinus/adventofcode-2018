15a0661ea1ceae8016a03640d1f17bbb4c6c17ed
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-07-14 12:41:44 +0200

    start writing the state

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -1,15 +1,60 @@
 mod encode_as_u8;
 mod input;
 
-use std::path::Path;
+use bitvec::vec::BitVec;
+use std::{
+    ops::{Deref, Index, IndexMut},
+    path::Path,
+};
 
 // each array of 5 bits corresponds to a single number in the range `0..32`,
 // so we can encode the complete ruleset as an array of 32 bools.
 pub type Rules = [bool; 32];
 
+struct State {
+    pots: BitVec,
+    zero_offset: isize,
+}
+
+impl Deref for State {
+    type Target = BitVec;
+
+    fn deref(&self) -> &Self::Target {
+        &self.pots
+    }
+}
+
+impl Index<isize> for State {
+    type Output = bool;
+
+    fn index(&self, index: isize) -> &Self::Output {
+        assert!(self.zero_offset + index >= 0, "index out of bounds");
+        self.pots.index((self.zero_offset + index) as usize)
+    }
+}
+
+impl State {
+    /// Set a bit of this state.
+    fn set(&mut self, index: isize, value: bool) {
+        todo!()
+    }
+
+    /// Enumerate over all windows of length 5 which are centered
+    /// on pots in this state. Treat them as bits of a value, and
+    /// return the value.
+    ///
+    /// The index returned is the offset of the center item in the window.
+    ///
+    /// Note that this produces 4 more items than `self.len()`: there
+    /// are two items overhanging on each side.
+    fn windows_enumerated(&self) -> impl Iterator<Item = (isize, u8)> {
+        todo!();
+        std::iter::empty()
+    }
+}
+
 pub fn part1(input: &Path) -> Result<(), Error> {
     let input = input::Input::load_file(input)?;
-    dbg!(&input);
     let rules = input.rules;
     unimplemented!()
 }
